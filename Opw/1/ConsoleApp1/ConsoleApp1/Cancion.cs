1f08cfc79bb1bc19f539038d6879eddf2f568d6b
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-10-22 17:02:26 +0200

    1

---
+++ Opw/1/ConsoleApp1/ConsoleApp1/Cancion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Cancion: IMedia
+    {
+        public long Seconds { get; set; }
+        public String Title { get; set; }
+        public ShelvePosition position { get; set; }
+
+
+        public void play()
+        {
+            Console.WriteLine($"This Song {this.Title} Is Playing");
+        }
+    }
+}
---
+++ Opw/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Cancion song = new Cancion();
+
+            song.Title = "ALgo";
+            song.Seconds = 400L;
+            song.position = new ShelvePosition(0, 0);
+
+            Console.WriteLine(song.Title);
+            Console.WriteLine(song.Seconds);
+            song.play();
+
+            Console.ReadKey();
+
+        }
+    }
+}
